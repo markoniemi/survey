91a2fbf3d5d9de55c4f896b91f2d6cf8859c9a21
Marko Niemi <Marko Niemi@192.168.1.34>
2014-10-23 20:23:15 +0300

    Fixes #11, Refactor and cleanup FileUploadBean. Fixes #2, Implement FileUploadBeanTest.

--- survey/survey-web/src/main/java/org/survey/user/bean/FileUploadBean.java
+++ survey/survey-web/src/main/java/org/survey/user/bean/FileUploadBean.java
@@ -16,17 +16,12 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.survey.file.model.File;
 import org.survey.file.service.FileService;
+import org.survey.user.FacesUtil;
 import org.survey.user.model.User;
 import org.survey.user.service.UserService;
 
 /**
- * FileUpload requires two context-parameter's to be set in web.xml:
- * 
- * createTempFiles boolean attribute which defines whether the uploaded files
- * are stored in temporary files or available in the listener directly as byte[]
- * data (false for this example). maxRequestSize attribute defines max size in
- * bytes of the uploaded files (1000000 for this example).
- * 
+ * FileUpload uses JSF 2.2 h:inputFile component.
  */
 @Slf4j
 @Component
@@ -44,54 +39,56 @@ public class FileUploadBean implements Serializable {
     private transient UserService userService;
     @Setter
     @Autowired
-    private transient UserBean userBean;
+    transient UserBean userBean;
     @Getter
     @Setter
     private Part file;
-    private byte[] fileContent;
+
     /**
      * Called when user presses Upload button in file upload dialog. Saves file
      * to FileService.
      */
-//    public void uploadFile(FileUploadEvent event) {
-//        File file = createFile(event.getUploadedFile());
-//        log.debug(file.toString());
-//        fileService.create(file);
-//    }
-
-    // TODO cleanup and refactor
     public void upload() {
-        log.debug("FileUploadBean.upload()");
         try {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            IOUtils.copy(file.getInputStream(), outputStream);
-            fileContent = outputStream.toByteArray();
-            log.debug("fileContent.size: {}", fileContent.length);
-            log.debug("file.name: {} .size: {}", file.getName(), file.getSize());
+            byte[] fileContent = getFileContent();
             File createdFile = createFile(file, fileContent);
             fileService.create(createdFile);
         } catch (IOException e) {
-            // Error handling
+            showMessage(null, "fileUploadError");
         }
     }
 
-    private User getUser() {
+    /**
+     * Get file as byte array from Part.
+     */
+    private byte[] getFileContent() throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        IOUtils.copy(file.getInputStream(), outputStream);
+        return outputStream.toByteArray();
+    }
+
+    protected User getUser() {
         return userService.findOne(userBean.getUsername());
     }
 
     /**
-     * Create a File from UploadedFile.
+     * Create a File from Part.
      */
-//    private File createFile(UploadedFile uploadedFile) {
     private File createFile(Part uploadedFile, byte[] fileContent) {
         File file = new File();
         file.setFilename(uploadedFile.getName());
         file.setMimeType(uploadedFile.getContentType());
         file.setContent(fileContent);
         file.setOwner(getUser());
-        // file.setOwner(userBean.getUsername());
         file.setCreateTime(System.currentTimeMillis());
         file.setSize(uploadedFile.getSize());
         return file;
+    }
+
+    /**
+     * showMessage is package private to enable overriding in a test case.
+     */
+    void showMessage(String id, String messageKey) {
+        FacesUtil.showMessage(id, messageKey);
     }
 }
--- survey/survey-web/src/test/java/org/survey/user/bean/FileUploadBeanTest.java
+++ survey/survey-web/src/test/java/org/survey/user/bean/FileUploadBeanTest.java
@@ -1,104 +1,137 @@
 package org.survey.user.bean;
 
-import org.junit.Ignore;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+
+import javax.servlet.http.Part;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Delegate;
+
+import org.apache.commons.collections.IteratorUtils;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.survey.file.model.File;
+import org.survey.file.repository.FileRepository;
+import org.survey.file.service.FileService;
+import org.survey.user.model.Role;
+import org.survey.user.model.User;
+import org.survey.user.repository.UserRepository;
 
 /**
  * Test FileUploadBean. Must use SpringJUnit4ClassRunner to enable spring
  * injection. Loaded Spring configuration is defined by ContextConfiguration.
  */
-@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:spring-config-test.xml")
 public class FileUploadBeanTest {
-//	@Autowired
-//	FileUploadBean fileUploadBean;
-//	@Autowired
-//	FileRepository fileRepository;
-//
-//	@Before
-//	public void setUp() {
-//		fileUploadBean.setUserBean(new UserBeanMock("username"));
-//	}
-//
-//	@After
-//	public void tearDown() {
-//		fileRepository.deleteAll();
-//	}
-//
-//	@Test
-//	public void uploadFile() throws Exception {
-//		UploadedFile uploadedFile = new UploadedFileMock("filename",
-//				"mimeType", "content".getBytes());
-//		FileUploadEvent fileUploadEvent = new FileUploadEvent(new UIInput(),
-//				uploadedFile);
-//		fileUploadBean.uploadFile(fileUploadEvent);
-//		Iterable<File> files = fileRepository.findAllByFilename("filename");
-//		Assert.assertEquals(1, IteratorUtils.toList(files.iterator()).size());
-//	}
-//
-//	public class UserBeanMock extends UserBean {
-//		private String username;
-//
-//		public UserBeanMock(String username) {
-//			this.username = username;
-//		}
-//
-//		@Override
-//		public String getUsername() {
-//			return username;
-//		}
-//	}
-//
-//	@Data
-//	@RequiredArgsConstructor
-//	private class UploadedFileMock implements UploadedFile {
-//		@NonNull
-//		private String name;
-//		@NonNull
-//		private String contentType;
-//		private long size;
-//		@NonNull
-//		private byte[] data;
-//
-//		@Override
-//		public byte[] getData() throws FileUploadException {
-//			return data;
-//		}
-//
-//		@Override
-//		public InputStream getInputStream() throws IOException {
-//			return null;
-//		}
-//
-//		@Override
-//		public void delete() throws IOException {
-//		}
-//
-//		@Override
-//		public void write(String fileName) throws IOException {
-//		}
-//
-//		@Override
-//		public String getHeader(String headerName) {
-//			return null;
-//		}
-//
-//		@Override
-//		public Collection<String> getHeaderNames() {
-//			return null;
-//		}
-//
-//		@Override
-//		public Collection<String> getHeaders(String headerName) {
-//			return null;
-//		}
-//
-//		@Override
-//		public String getParameterName() {
-//			return null;
-//		}
-//	}
+    private FileUploadBeanMock fileUploadBean;
+    @Autowired
+    private FileRepository fileRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private FileService fileService;
+
+    @Before
+    public void setUp() {
+        fileUploadBean = new FileUploadBeanMock();
+        User user = userRepository.save(new User("username", "", "", Role.ROLE_USER));
+        fileUploadBean.setUser(user);
+        fileUploadBean.setFileService(fileService);
+    }
+
+    @After
+    public void tearDown() {
+        fileRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void upload() throws Exception {
+        PartMock uploadedFile = new PartMock("filename", "mimeType", "content".getBytes());
+        fileUploadBean.setFile(uploadedFile);
+        fileUploadBean.upload();
+        Iterable<File> files = fileRepository.findAllByFilename("filename");
+        Assert.assertEquals(1, IteratorUtils.toList(files.iterator()).size());
+    }
+    
+    public class UserBeanMock extends UserBean {
+        private User user;
+
+        public UserBeanMock(User user) {
+            this.user = user;
+        }
+
+        @Override
+        public String getUsername() {
+            return user.getUsername();
+        }
+    }
+
+    @SuppressWarnings("serial")
+    private class FileUploadBeanMock extends FileUploadBean {
+        @Getter
+        @Setter
+        User user;
+        @Delegate
+        BeanTestHelper beanTestHelper = new BeanTestHelper();
+
+        @Override
+        void showMessage(String id, String messageKey) {
+            beanTestHelper.showMessage(id, messageKey);
+        }
+    }
+
+    @Data
+    @RequiredArgsConstructor
+    private class PartMock implements Part {
+        @NonNull
+        private String name;
+        @NonNull
+        private String contentType;
+        private long size;
+        @NonNull
+        private byte[] data;
+
+        @Override
+        public void delete() throws IOException {
+        }
+
+        @Override
+        public String getHeader(String headerName) {
+            return null;
+        }
+
+        @Override
+        public Collection<String> getHeaderNames() {
+            return null;
+        }
+
+        @Override
+        public Collection<String> getHeaders(String headerName) {
+            return null;
+        }
+
+        @Override
+        public InputStream getInputStream() throws IOException {
+            return new ByteArrayInputStream(data);
+        }
+
+        @Override
+        public void write(String fileName) throws IOException {
+        }
+    }
 }
