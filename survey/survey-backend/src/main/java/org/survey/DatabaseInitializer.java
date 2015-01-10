b841ada5dc9f86016db4796bf7c74a12e7df169b
Marko Niemi <Marko Niemi@192.168.1.40>
2015-01-10 11:15:32 +0200

    Unneccessary facets removed. Unneccessary database config removed. Database properties not in profiles, caused too much trouble with eclipse.

--- survey/survey-backend/src/main/java/org/survey/DatabaseInitializer.java
+++ survey/survey-backend/src/main/java/org/survey/DatabaseInitializer.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.survey.user.model.Role;
 import org.survey.user.model.User;
 import org.survey.user.service.UserService;
+
 @Slf4j
 public class DatabaseInitializer {
 
@@ -19,7 +20,10 @@ public class DatabaseInitializer {
     @PostConstruct
     public void initialize() {
         log.debug("DatabaseInitializer.initialize");
-        userService.create(new User("admin", "admin", "admin@test.com",
-                Role.ROLE_ADMIN));
+        try {
+            userService.create(new User("admin", "admin", "admin@test.com", Role.ROLE_ADMIN));
+        } catch (Exception e) {
+            log.warn(e.getLocalizedMessage());
+        }
     }
 }
