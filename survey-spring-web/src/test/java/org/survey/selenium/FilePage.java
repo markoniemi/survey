4c5e22650ff42ea7de4d33da667d664f761fd292
Marko Niemi <marko.j.niemi@gmail.com>
2018-12-30 11:27:00 +0200

    Fix for file separator.

--- survey-spring-web/src/test/java/org/survey/selenium/FilePage.java
+++ survey-spring-web/src/test/java/org/survey/selenium/FilePage.java
@@ -1,5 +1,7 @@
 package org.survey.selenium;
 
+import java.io.File;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -9,7 +11,7 @@ public class FilePage extends AbstractPage {
     }
 
     public void addFile(String filename) {
-        setText(By.id("file"), System.getProperty("user.dir") + "\\" + filename);
+        setText(By.id("file"), System.getProperty("user.dir") + File.separator + filename);
         click(By.id("submit"));
         assertTitle("Files");
     }
